ce6daa675f01f8be1cb96d506584fa679265c184
Iurii Prudius <iprudius@geeksforless.com>
2024-07-16 10:59:16 +0300

    cosmetic

--- tests/defines.h
+++ tests/defines.h
@@ -3,13 +3,27 @@
 
 #define _GNU_SOURCE
 
+/**
+ * Frees the allocated block, if it exists, 
+ * and sets the pointer to NULL
+ * 
+ */
 #define FREE_AND_NULL(x) \
     if (x)               \
         free(x);         \
     x = NULL
 
+/**
+ * Starts reading test data from the file 
+ * with the name of the current function 
+ * at the specified path  
+ * 
+ * The local variable `DATA_DIR` contains the specified path
+ * 
+ */
 #define START_USING_TEST_DATA(path)                                                   \
     {                                                                                 \
+        char *DATA_DIR = strdup(path);                                                \
         FILE *_fp;                                                                    \
         char *_pathstr = NULL;                                                        \
         asprintf(&_pathstr, "%s%s", (path), __func__);                                \
@@ -21,9 +35,19 @@
             {                                                                         \
                 if (strlen(_str) && _str[0] != '#')
 
+/**
+ * Scans variables from the current line, 
+ * like the `sscanf` function
+ * 
+ */
 #define USE_OF_THE_TEST_DATA(...) \
     sscanf(_str, __VA_ARGS__)
 
+/**
+ * Finishes reading test data 
+ * and frees all variables 
+ * 
+ */
 #define FINISH_USING_TEST_DATA                     \
                 FREE_AND_NULL(_str);               \
             }                                      \
@@ -37,6 +61,7 @@
             FREE_AND_NULL(_pathstr);               \
             CU_ASSERT_TRUE_FATAL(0);               \
         }                                          \
+        FREE_AND_NULL(DATA_DIR);                   \
     }
 
 #endif // UNIT_TEST_DEFS_H
