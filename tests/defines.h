ba2b8c647747774dbf9fcada38aa65f633c36215
Iurii Prudius <iprudius@geeksforless.com>
2024-07-16 10:12:07 +0300

    cosmetic

--- tests/defines.h
+++ tests/defines.h
@@ -5,35 +5,38 @@
 
 #define FREE_AND_NULL(x) \
     if (x)               \
-    free(x), x = NULL
+        free(x);         \
+    x = NULL
 
-#define START_USING_TEST_DATA(path)                        \
-    char *_str = NULL;                                     \
-    FILE *_fp;                                             \
-    asprintf(&_str, "%s%s", (path), __func__);             \
-    if ((_fp = fopen(_str, "r")))                          \
-    {                                                      \
-        free(_str);                                        \
-        _str = NULL;                                       \
-        size_t _len = 0;                                   \
-        int _read;                                         \
-        while ((_read = getline(&_str, &_len, _fp)) != -1) \
-        {                                                  \
-            if (strlen(_str) && _str[0] != '#')
+#define START_USING_TEST_DATA(path)                                                   \
+    {                                                                                 \
+        FILE *_fp;                                                                    \
+        char *_pathstr = NULL;                                                        \
+        asprintf(&_pathstr, "%s%s", (path), __func__);                                \
+        if ((_fp = fopen(_pathstr, "r")))                                             \
+        {                                                                             \
+            size_t _len = 0;                                                          \
+            char *_str = NULL;                                                        \
+            for (int _read = 0; (_read = getline(&_str, &_len, _fp)) != -1; _len = 0) \
+            {                                                                         \
+                if (strlen(_str) && _str[0] != '#')
 
-#define USE_OF_THE_TEST_DATA(...) sscanf(_str, __VA_ARGS__);
+#define USE_OF_THE_TEST_DATA(...) \
+    sscanf(_str, __VA_ARGS__)
 
-#define FINISH_USING_TEST_DATA             \
-    free(_str);                            \
-    _str = NULL;                           \
-    }                                      \
-    fclose(_fp);                           \
-    }                                      \
-    else                                   \
-    {                                      \
-        printf("'%s' not found...", _str); \
-        free(_str);                        \
-        CU_ASSERT_TRUE_FATAL(0);           \
+#define FINISH_USING_TEST_DATA                     \
+                FREE_AND_NULL(_str);               \
+            }                                      \
+            FREE_AND_NULL(_str);                   \
+            fclose(_fp);                           \
+            FREE_AND_NULL(_pathstr);               \
+        }                                          \
+        else                                       \
+        {                                          \
+            printf("'%s' not found...", _pathstr); \
+            FREE_AND_NULL(_pathstr);               \
+            CU_ASSERT_TRUE_FATAL(0);               \
+        }                                          \
     }
 
 #endif // UNIT_TEST_DEFS_H
